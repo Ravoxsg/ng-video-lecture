f82df94c6a747e39cd93a0073432756c8a44fa3a
Mathieu <mathieu.ravaut@gmail.com>
2024-01-05 12:09:33 +0800

    running on macbook

---
+++ engine.py
@@ -0,0 +1,27 @@
+import torch
+from tqdm import tqdm
+
+
+# data loading
+def get_batch(train_data, val_data, split, block_size, batch_size, device):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss(train_data, val_data, block_size, batch_size, model, device, eval_iters):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in tqdm(range(eval_iters)):
+            X, Y = get_batch(train_data, val_data, split, block_size, batch_size, device)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
